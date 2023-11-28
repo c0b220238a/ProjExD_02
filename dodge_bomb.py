27fbd6cb004d1d2ece03d29ef618a7ba5c2d3d80
akira <c0b220238a@edu.teu.ac.jp>
2023-11-28 17:16:18 +0900

    機能１途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,22 +2,15 @@ import random
 import sys
 import pygame as pg
 
-
 WIDTH, HEIGHT = 800, 600
-#練習３
 delta = {
-    pg.K_UP:(0, -5),
-    pg.K_DOWN:(0, +5),
-    pg.K_LEFT:(-5, 0),
-    pg.K_RIGHT:(+5, 0)
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
 }
-#練習４
+
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
-    """
-    オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
-    因数rct:こうかとんor爆弾Surfaceのrect
-    戻り値:横方向はみだし判定結果（画面内:true/画面外:false）
-    """
     yoko, tate = True, True
     if rct.left < 0 or WIDTH < rct.right:
         yoko = False
@@ -25,65 +18,75 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rect = kk_img.get_rect() 
+    original_kk_img = pg.image.load("ex02/fig/3.png")
+    kk_img = pg.transform.rotozoom(kk_img, 0, 1.0)
+    kk_rect = kk_img.get_rect()
     kk_rect.centerx = 400
     kk_rect.centery = 300
 
-    clock = pg.time.Clock()
-    #練習１爆弾作成
-    baku = pg.Surface((20,20))
-    pg.draw.circle(baku,(255,0,0), (10,10), 10)
-    baku.set_colorkey((0,0,0))
-    baku_rect = baku.get_rect() 
-    baku_rect.centerx = random.randint(0,WIDTH)
-    baku_rect.centery = random.randint(0,HEIGHT)
-    #練習２
+    baku = pg.Surface((20, 20))
+    pg.draw.circle(baku, (255, 0, 0), (10, 10), 10)
+    baku.set_colorkey((0, 0, 0))
+    baku_rect = baku.get_rect()
+    baku_rect.centerx = random.randint(0, WIDTH)
+    baku_rect.centery = random.randint(0, HEIGHT)
+
     vx, vy = +5, +5
 
-
-
+    clock = pg.time.Clock()
     tmr = 0
-
 
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
-            
+
         if kk_rect.colliderect(baku_rect):
             print("Game Over")
             return
 
-
-        #練習３
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, tpl in delta.items():
-            if key_lst[k]: #キーが押されたら＋＝が起きる
+            if key_lst[k]:
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
+
+        if sum_mv[0] > 0:  # 右キーが押されたとき
+            kk_img = pg.transform.flip(original_kk_img, True, False)
+        elif sum_mv[0] < 0:  # 左キーが押されたとき
+            kk_img = original_kk_img
+
+        if sum_mv[1] > 0:  # 下キーが押されたとき
+            kk_img = pg.transform.rotate(original_kk_img, 90)
+        elif sum_mv[1] < 0:  # 上キーが押されたとき
+            kk_img = original_kk_img
         
-        #練習４
+        if tuple(sum_mv) == (+5 , 0):  # 右キーが押されたとき
+            kk_img = pg.transform.rotozoom(kk_img, 45, 1.0)
+        if tuple(sum_mv) == (+5 , 0):  # 右キーが押されたとき
+            kk_img = pg.transform.rotozoom(kk_img, 135, 1.0)
+        if tuple(sum_mv) == (+5 , +5):  # 右キーが押されたとき
+            kk_img = pg.transform.rotozoom(kk_img, 315, 1.0)
+        if tuple(sum_mv) == (-5 , +5):  # 右キーが押されたとき
+            kk_img = pg.transform.rotozoom(kk_img, 225, 1.0)
+        if tuple(sum_mv) == (-5 , -5):  # 右キーが押されたとき
+            kk_img = pg.transform.rotozoom(kk_img, 135, 1.0)
+
         screen.blit(bg_img, [0, 0])
         kk_rect.move_ip(sum_mv[0], sum_mv[1])
         if check_bound(kk_rect) != (True, True):
             kk_rect.move_ip(-sum_mv[0], -sum_mv[1])
 
-
         screen.blit(kk_img, kk_rect)
-        #screen.blit(kk_img, [900, 400])
-        #練習１
         screen.blit(baku, baku_rect)
-        #練習２
+
         baku_rect.move_ip(vx, vy)
-        #練習４
         yoko, tate = check_bound(baku_rect)
         if not yoko:
             vx *= -1
@@ -91,13 +94,10 @@ def main():
             vy *= -1
         baku_rect.move_ip(vx, vy)
 
-
-
         pg.display.update()
 
         tmr += 1
         clock.tick(50)
-
 
 if __name__ == "__main__":
     pg.init()
